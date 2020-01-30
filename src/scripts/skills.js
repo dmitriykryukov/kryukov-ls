0c843d8e94d19d2e1f317398240db814a11fc0e1
Dmitriy Kryukov <kriukovdmitriy@gmail.com>
2020-01-30 00:17:11 -0800

    Implemented of the Skills block on Vuejs

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,53 @@
-console.log('this is skills module');
+import Vue from "vue";
+
+const skill = {
+  template: "#skill",
+  props: {
+    skillName: String,
+    skillPercent: Number
+  },
+  data() {
+    return {
+      animatDelay: ''
+    };    
+  },
+
+  created() {
+    const data =this.skillPercent;
+    this.animatDelay = data;
+  }
+
+}
+
+const skillsRow = {
+  template: "#skills-row",
+  components: {
+    skill
+  },
+  props: ["skill"]
+}
+
+new Vue({
+  el: "#skills-component",
+  template: "#skills-list",
+  components: {
+    skillsRow
+  },
+  data: {
+    color: 'red' 
+  },
+  
+
+  data() {
+    return {
+      skills: []
+    };    
+    
+  },
+
+  created() {
+    const data = require("../data/skills.json");
+    this.skills = data;
+  }
+});
+
