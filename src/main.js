a39c26e7306abcf43e847990463798ee16eac4f2
Dmitriy Kryukov <kriukovdmitriy@gmail.com>
2020-02-03 14:32:45 -0800

    Added parallax Mountains

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV === "development") {
 
 import "./scripts/skills";
 import "./scripts/works";
+import "./scripts/parallax";
 
 
 // window.onscroll = function() {
---
+++ src/scripts/parallax.js
@@ -0,0 +1,21 @@
+const parallax = document.querySelector('.parallax');
+const layers = parallax.children;
+
+function moveLayersDependsOnScroll(wScroll) {
+  Array.from(layers).forEach(layer => {
+
+    const divider = layer.dataset.speed;
+    console.log(divider);
+       
+    const strafe = wScroll*divider / 10;
+    
+
+    layer.style.transform = `translateY(-${strafe}%)`;
+
+  })
+}
+
+window.addEventListener('scroll', e => {
+  const wScroll = window.pageYOffset;
+  moveLayersDependsOnScroll(wScroll);
+})
