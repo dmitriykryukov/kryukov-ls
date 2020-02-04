f4db1abd653b1d857a07c4b2cba95bdfd114e6ed
Dmitriy Kryukov <kriukovdmitriy@gmail.com>
2020-02-04 10:48:06 -0800

    Added Parallax

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,21 +1,33 @@
 const parallax = document.querySelector('.parallax');
+const parallaxDown = document.querySelector('.parallax-down');
 const layers = parallax.children;
+const layersDown = parallaxDown.children;
 
-function moveLayersDependsOnScroll(wScroll) {
-  Array.from(layers).forEach(layer => {
+function moveLayersDependsOnScroll(wScroll, items) {
+  Array.from(items).forEach(item => {
 
-    const divider = layer.dataset.speed;
-    console.log(divider);
-       
+    const divider = item.dataset.speed;       
     const strafe = wScroll*divider / 10;
-    
+    item.style.transform = `translateY(-${strafe}%)`;
+  })
+}
 
-    layer.style.transform = `translateY(-${strafe}%)`;
+function moveLayersDependsOnScrollDown(wScroll, items) {
+  Array.from(items).forEach(item => {
 
+    const divider = item.dataset.speed;       
+    const strafe = (wScroll-5000)*divider / 10;
+    item.style.transform = `translateY(-${strafe}%)`;
   })
 }
 
 window.addEventListener('scroll', e => {
   const wScroll = window.pageYOffset;
-  moveLayersDependsOnScroll(wScroll);
+  if (wScroll > 3200) {
+    moveLayersDependsOnScrollDown(wScroll, layersDown);
+  } else {
+    moveLayersDependsOnScroll(wScroll, layers);
+  }
+  
+  
 })
