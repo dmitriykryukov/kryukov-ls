f35be6846932b090cd60afd4428cda9951186a93
Dmitriy Kryukov <kriukovdmitriy@gmail.com>
2020-02-19 01:17:11 -0800

    display reviews on the landing page

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
+var Flickity = require('flickity');
+
 import "./scripts/skills";
 import "./scripts/works";
 import "./scripts/parallax";
--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,85 +1,85 @@
 import Vue from "vue";
 import axios from "axios";
+// import EventBus from '../EventBus';
+import Flickity from 'vue-flickity';
 
 const $axios = axios.create ({
   baseURL: 'https://webdev-api.loftschool.com/'
 });
 
-const slideshow = {
-  template: "#slideshow-reviews",
-  props: ["reviews"],
-  
-  methods: {
-    reviewAuthorPic(review) {
-      const baseUrl = 'https://webdev-api.loftschool.com/';
-      return review && reviews.photo ? (baseUrl + review.photo) : '';
-    }
-  },
-}
-
 
 new Vue ({
   el: "#reviews-component",
   template: "#reviews-container",
-  components: { slideshow },
+  components: { Flickity },
 
   data() {
     return {
       reviews: [],
       activePage: 0,
+      slidesPerPage: 2,
+      pages: '',
+      flickityOptions: {
+        prevNextButtons: false,
+        pageDots: false,
+        groupCells: 2,
+        wrapAround: false,
+        cellAlign: "left",
+      }
     }
   },
 
   methods: {
-    makeArrWithRequiredImages(data) {
-      return data.map(item => {
-        const requirePic = require(`../images/content/${item.author_pic}`);
-        item.author_pic = requirePic;
-        return item;
-      });
+    getPages() {
+      if (this.slidesPerPage > 1) {
+        return this.reviews.length % 2 === 1 ? (this.reviews.length - 1) / 2 : (this.reviews.length / 2 - 1);
+      }
+      return this.reviews.length - 1;
     },
 
     slide(direction){
+      this.pages = this.getPages();
       switch(direction) {
         case "next":
-          flkty.next();
-          
+          this.$refs.flickity.next();
+          this.activePage = this.$refs.flickity.selectedIndex();
           break;
         case "prev":
-          flkty.previous();
+          this.$refs.flickity.previous();
+          this.activePage = this.$refs.flickity.selectedIndex();
           break;
       }
     },
 
+    calcSlidesPerPage(self) {
+      function calc() {
+        self.slidesPerPage = (window.innerWidth <= 768) ? 1 : 2;
+      }
+      calc();
+      window.addEventListener('resize', calc);
+    }
   },
+
+  mounted() {
+    this.calcSlidesPerPage(this);
+    this.flickityOptions.groupCells = this.slidesPerPage;
+  },
+
+  // watch: {
+  //   slidesPerPage() {
+  //     EventBus.$emit('pages', this.pages());
+  //   }
+  // },
 
 
   async created() {
     const { data } = await $axios.get("/reviews/282");
-    this.reviews = data;
-    // const data = require("../data/reviews.json");
-    // this.reviews = this.makeArrWithRequiredImages(data);
+    this.reviews = data.map(item => {
+          item.photo = "https://webdev-api.loftschool.com/" + item.photo;
+          return item;
+    });
+
+    this.$nextTick(function () { this.$refs.flickity.rerender() });
   }
 
-});
-
-
-
-// Flickity SLIDER
-
-var elem = document.querySelector('.reviews-slideshow');
-var flkty = new Flickity( elem, {
-  
- cellAlign: 'left',
- pageDots: false,
- wrapAround: false,
- initialIndex: 2,
-
-//  initialIndex: 3,
-//         prevNextButtons: true,
-//         pageDots: true,
-//         wrapAround: true,
-//         freeScroll: true
-
-});
-
+})
