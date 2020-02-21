8bb1bfe23ddcf384bf38d4075e86fa8c4036e617
Dmitriy Kryukov <kriukovdmitriy@gmail.com>
2020-02-21 11:29:46 -0800

    added test#1

--- test/admin.login.spec.js
+++ test/admin.login.spec.js
@@ -9,42 +9,42 @@ beforeAll(() => {
   return client.init().url('http://localhost:8080/admin#/login');
 });
 
-// test('На странице есть кнопка авторизации', () => {
-//   return client
-//     .isExisting('.btn--login')
-//     .then(browsers => {
-//       for (const browserName in browsers) {
-//         expect(browsers[browserName]).toBe(true);
-//       }
-//     })
-//     .screenshot()
-//     .then(browsers => {
-//       for (const browserName in browsers) {
-//         fs.writeFileSync(
-//           `./screenshots/login_form_${browserName}_has_button_authorization.png`,
-//           browsers[browserName].value,
-//           'base64',
-//         );
-//       }
-//     });
-// });
+test('Тест1. При клике на кнопку появляется форма', () => {
+  return client
+    .isExisting('.login__form')
+    .then(browsers => {
+      for (const browserName in browsers) {
+        expect(browsers[browserName]).toBe(true);
+      }
+    })
+    .screenshot()
+    .then(browsers => {
+      for (const browserName in browsers) {
+        fs.writeFileSync(
+          `./screenshots/login_form_${browserName}_has_button_authorization.png`,
+          browsers[browserName].value,
+          'base64',
+        );
+      }
+    });
+});
 
-// test('Тест 2. Форма имеет все необходимые поля', () => {
-//   return client
-//       .isExisting('.input__icon--user')
-//       .isExisting('.input__icon--key')
-//       .then(browsers => {
-//           for (const browserName in browsers) {
-//               expect(browsers[browserName]).toBe(true);
-//           }
-//       })
-//       .screenshot()
-//       .then(screenshots => {
-//           for (const browserName in screenshots) {
-//               fs.writeFileSync(`./screenshots/login_form_${browserName}_has_input-form.png`, screenshots[browserName].value, 'base64');
-//           }
-//       })
-// });
+test('Тест 2. Форма имеет все необходимые поля', () => {
+  return client
+      .isExisting('.input__icon--user')
+      .isExisting('.input__icon--key')
+      .then(browsers => {
+          for (const browserName in browsers) {
+              expect(browsers[browserName]).toBe(true);
+          }
+      })
+      .screenshot()
+      .then(screenshots => {
+          for (const browserName in screenshots) {
+              fs.writeFileSync(`./screenshots/login_form_${browserName}_has_input-form.png`, screenshots[browserName].value, 'base64');
+          }
+      })
+});
 
 test('Тест 3. Кнопка “Отправить” заблокирована до тех пор, пока не введены все необходимые данные', () => {
   return client
